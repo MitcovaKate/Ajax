dff44d2ecf711013fee592be3defa17be58b4d22
Mitcova Kate <katya.mitkova@bk.ru>
2024-02-20 22:08:56 +0200

    Update app.js

--- captcha/app.js
+++ captcha/app.js
@@ -1,10 +1,47 @@
-    const breakCapcha = (capcha, cb) => {
-        setTimeout(() => {
-          if (typeof capcha !== 'string') {
-            throw new TypeError('This function accepts only STRING captchas!');
-          }
+   //1 promise cb
+//     const breakCapcha = (capcha, cb) => {
+//       if (typeof capcha !== 'string') {
+//             cb(null,'This function accepts only STRING captchas!');
+//           }
+          
+//         setTimeout(() => {
+//           let solution = capcha.split("");
+//           cb(solution);
+//         }, 1000);
+//       };
+
+//       const render=(solution, err)=>{
+//         if(err)
+//         result.innerHTML=`${err}`
+//       else
+//         result.innerHTML = `${solution.join(' ')}`
+//       }
       
-          let solution = capcha.split("");
-          cb(solution);
-        }, 1000);
-      };
+        
+       
+// breakCapcha("abc123",render)
+
+//2 promise constructor
+
+const breakCapcha=(captcha)=>{
+  return new Promise((resolve, reject)=>{
+    if(typeof captcha !=='string')
+  return  reject('This function accepts only STRING captchas!')
+
+  // breacking
+
+  setTimeout(()=>{
+    let solution=captcha.split('')
+    resolve(solution)
+  },1000)
+  })
+}
+const renderRersult=(solution)=>{
+  result.innerHTML=`${solution.join(' ')}`
+}
+const renderError=(err)=>{
+  result.innerHTML=`${err}`
+}
+breakCapcha(null)
+  .then(renderRersult)
+  .catch(renderError)
